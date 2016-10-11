ba0beb7081d4d7149fda73f717a574100b1dce41
ShMcK <shawn.j.mckay@gmail.com>
2016-10-11 12:48:18 -0700

    outline combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,10 @@ import R from 'ramda'
 import filterReducer from './filter'
 import todoReducer from './todo'
 
-const reducers = 
-  filterReducer
-    .concat(todoReducer)
-    .map(s => R.merge(s, { lastUpdated: Date() }))
+const combineReducers = R.compose(x => R.tail(x).reduce((a, b) => a.concat(b), R.head(x)), R.values)
 
-export default reducers
+export default combineReducers({
+  todos: todoReducer,
+  filter: filterReducer,
+})
+  .map(s => R.merge(s, { lastUpdated: Date() }))
